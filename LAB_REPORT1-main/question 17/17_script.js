51187e3fe20087b9819347687f6ae676210aa29b
SamyakOe <samyakoe@gmail.com>
2025-06-23 14:04:42 +0545

    upto 17

--- LAB_REPORT1-main/question 17/17_script.js
+++
@@ -1,6 +0,0 @@
-const mobileMenu = document.getElementById('mobile-menu');
-const menu = document.querySelector('.menu');
-
-mobileMenu.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
